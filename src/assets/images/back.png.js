f934cf2b637f8731a36fac98b43785e50adcf033
Faridun Ismoilov <faridun.ismoilov@swfx.ch>
2025-04-06 02:32:45 +0500

    ffffff

--- src/assets/images/back.png.js
+++ src/assets/images/back.png.js
@@ -1,2 +1,3 @@
 // Это заглушка для изображения, которое нужно будет добавить позже
-export default 'https://images.unsplash.com/photo-1589882082073-76ae911e3c72?q=80&w=2070&auto=format&fit=crop'; 
+const backImageUrl = 'https://images.unsplash.com/photo-1589882082073-76ae911e3c72?q=80&w=2070&auto=format&fit=crop';
+export default backImageUrl; 
